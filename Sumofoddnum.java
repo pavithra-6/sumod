2f85a8f072237757aa55306450bdefb655612773
pavithra-6 <paviikannan1996@gmail.com>
2017-07-04 14:57:13 +0530

    Add files via upload

---
+++ Sumofoddnum.java
@@ -0,0 +1,17 @@
+package sumave1;
+
+public class Sumofoddnum {
+
+	public static void main(String[] args) {
+		int n=20;
+		int sum=0;
+for(int i=0;i<20;i++){
+	if(i%2!=0){
+sum+=i;
+	}
+}
+System.out.println(sum);
+
+	}
+
+}
